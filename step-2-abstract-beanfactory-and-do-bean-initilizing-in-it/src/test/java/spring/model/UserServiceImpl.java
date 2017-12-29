4ae2026bd6189c05c7e24a618579192884fbeb30
limengyu1990 <limengyu1990@163.com>
2017-12-29 17:13:03 +0800

    自动装配Bean

---
+++ step-2-abstract-beanfactory-and-do-bean-initilizing-in-it/src/test/java/spring/model/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package spring.model;
+
+/**
+ * @author limengyu
+ * @create 2017/12/29
+ */
+public class UserServiceImpl {
+
+    public void say(){
+
+        System.out.println("hello world");
+    }
+}
