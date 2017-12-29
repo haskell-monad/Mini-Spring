d162e564cf56bfb5d95621346447a7ed8b77fe2f
limengyu1990 <limengyu1990@163.com>
2017-12-29 18:24:58 +0800

    init

---
+++ step-3-inject-bean-with-property/src/main/java/spring/factory/AutowireCapableBeanFactory.java
@@ -0,0 +1,23 @@
+package spring.factory;
+
+import spring.BeanDefinition;
+
+/**
+ * @author limengyu
+ * @create 2017/12/29
+ */
+public class AutowireCapableBeanFactory extends AbstractBeanFactory{
+
+    @Override
+    public Object doCreateBean(BeanDefinition beanDefinition) {
+        try {
+            Object bean = beanDefinition.getBeanClass().newInstance();
+            return bean;
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ step-3-inject-bean-with-property/src/test/java/spring/BeanFactoryTest.java
@@ -0,0 +1,27 @@
+package spring;
+
+import org.junit.Test;
+
+import spring.factory.AutowireCapableBeanFactory;
+import spring.factory.BeanFactory;
+import spring.model.UserServiceImpl;
+
+/**
+ * @author limengyu
+ * @create 2017/12/29
+ */
+public class BeanFactoryTest {
+
+    @Test
+    public void getBeanTest(){
+        //构建bean容器
+        BeanFactory beanFactory = new AutowireCapableBeanFactory();
+        //注册bean
+        BeanDefinition beanDefinition = new BeanDefinition();
+        beanDefinition.setBeanClassName("spring.model.UserServiceImpl");
+        beanFactory.registerBean("user",beanDefinition);
+        //获取bean
+        UserServiceImpl user = (UserServiceImpl)beanFactory.getBean("user");
+        user.say();
+    }
+}
